8a4680bea283f25d63478782391e2b07a4aaa85b
koichi-tanaka <135241317+tana-51@users.noreply.github.com>
2024-04-23 15:50:14 +0900

    update

---
+++ opl/main_n_unique_actions_opl.py
@@ -0,0 +1,126 @@
+from omegaconf import DictConfig, OmegaConf
+import hydra
+import os
+
+from dataset import SyntheticCombinatorialBanditDataset
+from obp.dataset import linear_reward_function
+from pandas import DataFrame
+import pandas as pd
+from sklearn.neural_network import MLPRegressor
+from obp.ope import RegressionModel
+from sklearn.utils import check_random_state
+from tqdm import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from policyleaners import (
+    GradientBasedPolicyLearner,
+    RegBasedPolicyLearner,
+    OPCB,
+)
+from learner import train_reward_model_via_two_stage
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+@hydra.main(config_path="../conf",config_name="config_n_unique_actions_opl")
+def main(cfg: DictConfig) -> None:
+
+    num_runs = cfg.num_runs
+    epoch = cfg.epoch
+    num_data = cfg.num_data
+    num_unique_actions_list = cfg.num_unique_actions_list
+    random_state = cfg.random_state
+    random_ = check_random_state(random_state)
+
+    result_df_list = []
+    result_df = DataFrame()
+    for num_unique_action in num_unique_actions_list:
+        test_policy_value_list = []
+        
+        dataset = SyntheticCombinatorialBanditDataset(
+            n_unique_action=num_unique_action,
+            dim_context=cfg.dim_context,
+            base_reward_function=linear_reward_function,
+            #behavior_policy_function=linear_behavior_policy,
+            reward_type='continuous',
+            random_state=cfg.random_state,
+            reward_std=cfg.reward_std ,
+            beta=cfg.beta,
+            lambda_=cfg.lambda_,
+        )
+        
+        n_comb_action = 2**(dataset.n_unique_action)
+
+        test_bandit_data = dataset.obtain_batch_bandit_feedback(n_rounds=cfg.n_rounds_test_bandit_data,
+                                                                n_users=cfg.n_users,
+                                                                true_element=cfg.true_element,
+                                                                )
+
+        pi_0_value = dataset.calc_ground_truth_policy_value(
+                    expected_reward=test_bandit_data["expected_reward_matrix"], 
+                    action_dist=test_bandit_data["all_pscore"][:,:,np.newaxis],
+                )
+        
+        for _ in tqdm(range(num_runs), desc=f"num_unique_action={num_unique_action}..."):
+            
+            test_value_of_learned_policies = dict()
+            
+            train_bandit_data = dataset.obtain_batch_bandit_feedback(n_rounds=num_data,
+                                                                     n_users=cfg.n_users,
+                                                                     true_element=cfg.true_element,
+                                                                     )
+
+
+            ips = GradientBasedPolicyLearner(dim_context = cfg.dim_context, n_action=n_comb_action, epoch = epoch)
+            ips.fit(dataset=train_bandit_data, dataset_test=test_bandit_data)
+            pi_ips = ips.predict(test_bandit_data)
+            ips_value = (test_bandit_data["expected_reward_matrix"] * pi_ips).sum(1).mean()
+            test_value_of_learned_policies["ips"] = ips_value
+            
+            reg = RegBasedPolicyLearner(dim_context = cfg.dim_context, n_action=n_comb_action, epoch = epoch)
+            reg.fit(dataset=train_bandit_data, dataset_test=test_bandit_data)
+            pi_reg = reg.predict(test_bandit_data)
+            reg_value = (test_bandit_data["expected_reward_matrix"] * pi_reg).sum(1).mean()
+            test_value_of_learned_policies["reg"] = reg_value
+
+            q_hat = train_bandit_data["expected_reward_matrix"] + random_.normal(scale=cfg.q_hat_scale, size=(num_data, n_comb_action))
+            dr = GradientBasedPolicyLearner(dim_context = cfg.dim_context, n_action=n_comb_action, epoch = epoch)
+            dr.fit(dataset=train_bandit_data, dataset_test=test_bandit_data, q_hat=q_hat)
+            pi_dr = dr.predict(test_bandit_data)
+            dr_value = (test_bandit_data["expected_reward_matrix"] * pi_dr).sum(1).mean()
+            test_value_of_learned_policies["dr"] = dr_value
+
+
+    
+            opcb = OPCB(dim_context = cfg.dim_context, n_action=n_comb_action, epoch = epoch, kth_element=cfg.true_element,)
+            opcb.fit(dataset=train_bandit_data, dataset_test=test_bandit_data, q_hat=q_hat)
+            pi_opcb = opcb.predict(test_bandit_data)
+            opcb_value = (test_bandit_data["expected_reward_matrix"] * pi_opcb).sum(1).mean()
+            test_value_of_learned_policies["opcb"] = opcb_value
+            
+
+
+            test_policy_value_list.append(test_value_of_learned_policies)
+            
+        result_df = DataFrame(test_policy_value_list).stack().reset_index(1)\
+            .rename(columns={"level_1": "method", 0: "value"})
+        result_df["num_unique_action"] = num_unique_action
+        result_df["pi_0_value"] = pi_0_value
+        result_df["rel_value"] = result_df["value"] / pi_0_value
+        result_df_list.append(result_df)
+
+        tqdm.write("=====" * 15)
+        
+        
+    # aggregate all results 
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    os.chdir('../')
+    os.chdir('result')
+    result_df = pd.concat(result_df_list).reset_index(level=0)
+    result_df.to_csv("n_unique_actions_opl.csv")
+
+
+if __name__ == "__main__":
+    main()
---
+++ opl/policyleaners.py
@@ -0,0 +1,359 @@
+from torch.utils.data import Dataset
+from torch import nn
+import numpy as np
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+from tqdm import tqdm
+from dataclasses import dataclass
+from utils import (
+    GradientBasedPolicyDataset,
+    RegBasedPolicyDataset,
+)
+from typing import List
+from obp.utils import softmax
+
+seed = 12345
+# Numpy
+np.random.seed(seed)
+# Pytorch
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+torch.backends.cudnn.deterministic = True
+torch.manual_seed(seed)
+
+@dataclass
+class RegBasedPolicyLearner:
+    dim_context : int
+    n_action : int
+    batch_size : int = 16
+    epoch : int = 30
+    imit_reg: float = 0.0
+    log_eps: float = 1e-10
+    
+    def __post_init__(self, ):
+        seed = 12345
+        # Numpy
+        np.random.seed(seed)
+        # Pytorch
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.manual_seed(seed)
+        
+        self.nn_model = nn.Sequential(
+            nn.Linear(self.dim_context, 30),
+            nn.ReLU(),
+            nn.Linear(30, 30),
+            nn.ReLU(),
+            nn.Linear(30, self.n_action)
+        )
+        
+        self.train_loss = []
+        self.train_value = []
+        self.test_value = []
+    
+    def fit(self,dataset, dataset_test):
+        
+            
+        mydataset = RegBasedPolicyDataset(
+            context = dataset["context"], 
+            action = dataset["action"], 
+            reward = dataset["reward"], 
+        )
+        
+        train_dataloader = DataLoader(mydataset, batch_size=self.batch_size)
+        optimizer = torch.optim.Adam(self.nn_model.parameters(), lr=0.001)
+        q_x_a_train, q_x_a_test = dataset["expected_reward_matrix"], dataset_test["expected_reward_matrix"]
+        
+        
+        for e in range(self.epoch):
+            self.nn_model.train()
+            
+            for x_,a_,r_ in train_dataloader:
+                optimizer.zero_grad()
+                q_hat = self.nn_model(x_)
+                idx = torch.arange(a_.shape[0], dtype=torch.long)
+                loss = ((r_ - q_hat[idx, a_]) ** 2).mean()
+                loss.backward()
+                optimizer.step()
+            
+            pi_train = self.predict(dataset)
+            self.train_value.append((q_x_a_train * pi_train).sum(1).mean())
+            pi_test = self.predict(dataset_test)
+            self.test_value.append((q_x_a_test * pi_test).sum(1).mean())
+            #self.train_loss.append(loss_epoch)
+
+    def predict(self, dataset_test: np.ndarray, beta: float = 10):
+        self.nn_model.eval()
+        x = torch.from_numpy(dataset_test["context"]).float()
+        q_hat = self.nn_model(x).detach().numpy()
+
+        return softmax(beta * q_hat)
+    
+    def predict_q_hat(self, dataset_test):
+        self.nn_model.eval()
+        x = torch.from_numpy(dataset_test["context"]).float()
+
+        return self.nn_model(x).detach().numpy()
+    
+    
+    
+@dataclass
+class GradientBasedPolicyLearner:
+    dim_context : int
+    n_action : int
+    batch_size : int = 16
+    epoch : int = 30
+    imit_reg: float = 0.0
+    log_eps: float = 1e-10
+    
+    def __post_init__(self, ):
+        seed = 12345
+        # Numpy
+        np.random.seed(seed)
+        # Pytorch
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.manual_seed(seed)
+        
+        self.nn_model = nn.Sequential(
+            nn.Linear(self.dim_context, 30),
+            nn.ReLU(),
+            nn.Linear(30, 30),
+            nn.ReLU(),
+            nn.Linear(30, self.n_action),
+            nn.Softmax(dim=1)
+        )
+        
+        self.train_loss = []
+        self.train_value = []
+        self.test_value = []
+    
+    def fit(self,dataset, dataset_test, q_hat: np.ndarray = None):
+        
+        if q_hat is None:
+            q_hat = np.zeros((dataset["context"].shape[0], self.n_action))
+            
+        mydataset = GradientBasedPolicyDataset(
+            context = dataset["context"], 
+            action = dataset["action"], 
+            reward = dataset["reward"], 
+            pscore = dataset["pscore"], 
+            q_hat = q_hat, 
+            pi_0 = dataset["all_pscore"], 
+        )
+        
+        train_dataloader = DataLoader(mydataset, batch_size=self.batch_size)
+        optimizer = torch.optim.Adam(self.nn_model.parameters(), lr=0.001)
+        q_x_a_train, q_x_a_test = dataset["expected_reward_matrix"], dataset_test["expected_reward_matrix"]
+        
+        
+        for e in range(self.epoch):
+            self.nn_model.train()
+            
+            for x_,a_,r_,p,q_hat_,pi_0_ in train_dataloader:
+                optimizer.zero_grad()
+                pi = self.nn_model(x_)
+                loss = -self._estimate_policy_gradient(
+                    a=a_,
+                    r=r_,
+                    pscore=p,
+                    q_hat=q_hat_,
+                    pi_0=pi_0_,
+                    pi=pi,
+                ).mean()
+                loss.backward()
+                optimizer.step()
+            
+            pi_train = self.predict(dataset)
+            self.train_value.append((q_x_a_train * pi_train).sum(1).mean())
+            pi_test = self.predict(dataset_test)
+            self.test_value.append((q_x_a_test * pi_test).sum(1).mean())
+            #self.train_loss.append(loss_epoch)
+    
+    def _estimate_policy_gradient(
+        self,
+        a: torch.Tensor,
+        r: torch.Tensor,
+        pscore: torch.Tensor,
+        q_hat: torch.Tensor,
+        pi: torch.Tensor,
+        pi_0: torch.Tensor,
+    ) -> torch.Tensor:
+        current_pi = pi.detach()
+        log_prob = torch.log(pi + self.log_eps)
+        idx = torch.arange(a.shape[0], dtype=torch.long)
+
+        q_hat_factual = q_hat[idx, a]
+        iw = current_pi[idx, a] / pscore
+        estimated_policy_grad_arr = iw * (r - q_hat_factual) * log_prob[idx, a]
+        estimated_policy_grad_arr += torch.sum(q_hat * current_pi * log_prob, dim=1)
+
+        # imitation regularization
+        estimated_policy_grad_arr += self.imit_reg * log_prob[idx, a]
+
+        return estimated_policy_grad_arr
+
+    def predict(self, dataset_test: np.ndarray) -> np.ndarray:
+
+        self.nn_model.eval()
+        x = torch.from_numpy(dataset_test["context"]).float()
+        return self.nn_model(x).detach().numpy()
+    
+
+@dataclass
+class OPCB:
+    dim_context : int
+    n_action : int
+    kth_element : List[List[int]]
+    batch_size : int = 16
+    epoch : int = 30
+    imit_reg: float = 0.0
+    log_eps: float = 1e-10
+    
+    def __post_init__(self, ):
+        seed = 12345
+        # Numpy
+        np.random.seed(seed)
+        # Pytorch
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.manual_seed(seed)
+        
+        self.nn_model = nn.Sequential(
+            nn.Linear(self.dim_context, 30),
+            nn.ReLU(),
+            nn.Linear(30, 30),
+            nn.ReLU(),
+            nn.Linear(30, self.n_action),
+            nn.Softmax(dim=1)
+        )
+        
+
+        self.train_loss = []
+        self.train_value = []
+        self.test_value = []
+    
+    def fit(self,dataset, dataset_test, q_hat: np.ndarray = None):
+        
+        if q_hat is None:
+            q_hat = np.zeros((dataset["context"].shape[0], self.n_action))
+            
+        
+        self.action_context = dataset["action_context"]
+            
+        mydataset = GradientBasedPolicyDataset(
+            context = dataset["context"], 
+            action = dataset["action"], 
+            reward = dataset["reward"], 
+            pscore = dataset["pscore"], 
+            q_hat = q_hat, 
+            pi_0 = dataset["all_pscore"], 
+        )
+        
+        train_dataloader = DataLoader(mydataset, batch_size=self.batch_size)
+        optimizer = torch.optim.Adam(self.nn_model.parameters(), lr=0.001)
+        q_x_a_train, q_x_a_test = dataset["expected_reward_matrix"], dataset_test["expected_reward_matrix"]
+        
+        
+        for e in range(self.epoch):
+            self.nn_model.train()
+            
+            for x_,a_,r_,p,q_hat_,pi_0_ in train_dataloader:
+                optimizer.zero_grad()
+                pi = self.nn_model(x_)
+                loss = -self._estimate_policy_gradient(
+                    a=a_,
+                    r=r_,
+                    pscore=p,
+                    q_hat=q_hat_,
+                    pi_0=pi_0_,
+                    pi=pi,
+                ).mean()
+                loss.backward()
+                optimizer.step()
+            
+            pi_train = self.predict(dataset)
+            self.train_value.append((q_x_a_train * pi_train).sum(1).mean())
+            pi_test = self.predict(dataset_test)
+            self.test_value.append((q_x_a_test * pi_test).sum(1).mean())
+            #self.train_loss.append(loss_epoch)
+    
+    def _estimate_policy_gradient(
+        self,
+        a: torch.Tensor,
+        r: torch.Tensor,
+        pscore: torch.Tensor,
+        q_hat: torch.Tensor,
+        pi: torch.Tensor,
+        pi_0: torch.Tensor,
+    ) -> torch.Tensor:
+        current_pi = pi.detach()
+        log_prob = torch.log(pi + self.log_eps)
+        idx = torch.arange(a.shape[0], dtype=torch.long)
+
+        q_hat_factual = q_hat[idx, a]
+        
+#         #iw for OPCB
+#         n = a.shape[0]
+        
+#         kth_action_context = self.action_context[:,self.kth_element]
+        
+#         id_list = []
+#         for i in a:
+#             id_ = np.where(np.all(kth_action_context==kth_action_context[i,:],axis=1))
+#             id_list.append(id_[0].tolist())
+        
+#         pscore_OPCB = np.zeros(n)
+#         action_dist_OPCB = np.zeros(n)
+
+#         pos_ = 0
+#         for i in id_list:
+#             pscore_OPCB[pos_]=pi_0[pos_,i].sum()
+#             action_dist_OPCB[pos_] = current_pi[pos_,i].sum()
+#             pos_ += 1
+        
+#         iw_opcb = action_dist_OPCB / pscore_OPCB
+#         iw_opcb = torch.from_numpy(iw_opcb).float()
+
+        #iw for OPCB
+        n = a.shape[0]
+        
+        kth_action_context = self.action_context[:,self.kth_element]
+        
+        id_list = []
+        for i in a:
+            id_ = np.where(np.all(kth_action_context==kth_action_context[i,:],axis=1))
+            id_list.append(id_[0].tolist())
+        
+
+        pscore_OPCB = torch.zeros(n)
+        action_dist_OPCB = torch.zeros(n)
+        pi_OPCB = torch.zeros(n)
+
+        pos_ = 0
+        for i in id_list:
+            pscore_OPCB[pos_]=torch.sum(pi_0[pos_,i])
+            action_dist_OPCB[pos_] = torch.sum(current_pi[pos_,i])
+            pi_OPCB[pos_] = torch.sum(pi[pos_,i])
+            pos_ += 1
+            
+        
+        log_prob_OPCB = torch.log(pi_OPCB + self.log_eps)
+        iw_opcb = action_dist_OPCB / pscore_OPCB
+        #estimated_policy_grad_arr = iw_opcb * (r - q_hat_factual) * log_prob[idx, a]
+        estimated_policy_grad_arr = iw_opcb * (r - q_hat_factual) * log_prob_OPCB
+        estimated_policy_grad_arr += torch.sum(q_hat * current_pi * log_prob, dim=1)
+
+        # imitation regularization
+        estimated_policy_grad_arr += self.imit_reg * log_prob[idx, a]
+
+        return estimated_policy_grad_arr
+
+    def predict(self, dataset_test: np.ndarray) -> np.ndarray:
+
+        self.nn_model.eval()
+        x = torch.from_numpy(dataset_test["context"]).float()
+        return self.nn_model(x).detach().numpy()
